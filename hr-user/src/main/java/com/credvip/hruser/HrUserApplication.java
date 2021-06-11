d5d85816f66c632679409b45ca6552db0c3a5698
heloisaGuimaraes <heloisaguimaraes86@gmail.com>
2021-06-11 17:21:22 -0300

    User, Role and Many to many association

--- hr-user/src/main/java/com/credvip/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/credvip/hruser/HrUserApplication.java
@@ -1,4 +1,4 @@
-package com.credvip.hrconfigserver;
+package com.credvip.hruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ hr-user/src/main/java/com/credvip/hruser/entities/Role.java
@@ -0,0 +1,76 @@
+package com.credvip.hruser.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//Mapeamento JPA
+@Entity // Entidade monitorada por JPA
+@Table(name = "tb_role")
+public class Role implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String roleName;
+	
+	public Role() {
+		super();
+	}
+
+	public Role(Long id, String rolename) {
+		super();
+		this.id = id;
+		this.roleName = rolename;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRolename() {
+		return roleName;
+	}
+
+	public void setRolename(String rolename) {
+		this.roleName = rolename;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((roleName == null) ? 0 : roleName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (roleName == null) {
+			if (other.roleName != null)
+				return false;
+		} else if (!roleName.equals(other.roleName))
+			return false;
+		return true;
+	}
+	
+	
+
+}
