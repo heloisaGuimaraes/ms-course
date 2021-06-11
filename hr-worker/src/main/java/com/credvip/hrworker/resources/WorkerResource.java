e08c532aa4a651c277907053158b94f4b444fc23
heloisaGuimaraes <heloisaguimaraes86@gmail.com>
2021-06-11 15:49:36 -0300

    hr-worker as a config sever client

--- hr-worker/src/main/java/com/credvip/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/credvip/hrworker/resources/WorkerResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,6 +29,15 @@ public class WorkerResource {
 	private WorkerRepository repository; // ligação com a interface do bd
 	
 	
+	@Value("${test.config}")
+	private String testConfig;
+	
+	@GetMapping(value="/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG = "+testConfig);
+		return ResponseEntity.noContent().build();
+	}
+
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = repository.findAll();
