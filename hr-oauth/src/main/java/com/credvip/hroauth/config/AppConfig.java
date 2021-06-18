aa9173bf0a44f13af3f22d3f43b8680e122e1ccc
heloisaGuimaraes <heloisaguimaraes86@gmail.com>
2021-06-18 09:52:45 -0300

    Security configs to config server

--- hr-oauth/src/main/java/com/credvip/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/credvip/hroauth/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.credvip.hroauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,7 +9,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
-
+	
+	
+	@Value("${jwt.secret}") 
+	private String jwtSecret;
+	
+	
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -18,7 +24,7 @@ public class AppConfig {
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("MY-SECRET-KEY");
+		tokenConverter.setSigningKey(jwtSecret);
 		return tokenConverter;
 	}
 
